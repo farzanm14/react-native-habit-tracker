f879e53cb1b8fc83a804d2d95e59332220001a0c
farzaneh.mirzakhanloo <farzaneh.mirzakhanloo@gmail.com>
2025-09-05 14:10:34 +0330

    chore: init new project

---
+++ README.md
@@ -0,0 +1,10 @@
+Habit Tracker App
+
+ToDo: add these sections
+
+- Description about features of the app.
+- Steps to run
+- Overall approach
+- Tools and technologies
+- Demo
+- Folder structure
---
+++ app/_layout.tsx
@@ -0,0 +1,31 @@
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import "react-native-reanimated";
+
+export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
+  return (
+    <>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="auto" />
+    </>
+  );
+}
